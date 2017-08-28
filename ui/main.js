4ae0af32e3ecd0987308ae29b6931873ddf6695b
acharya221b <30840365+acharya221b@users.noreply.github.com>
2017-08-28 19:21:25 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,7 +21,6 @@ img.onclick = function() {
 var button = document.getElementById('counter');
 
 button.onclick = function () {
-    
   //Create a request object
   var request =  new XMLHttpRequest();
   
@@ -29,8 +28,8 @@ button.onclick = function () {
   request.onreadystatechange = function () {
       if(request.readyState === XMLHttpRequest.DONE) {
           //Take some action
-          if(request.status == 200) {
-                request.responseText;
+          if(request.status === 200) {
+                var counter = request.responseText;
                 var span = document.getElementById('count');
                 span.innerHTML = counter.toString();
           }
