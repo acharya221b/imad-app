888643b417f953c04d22e91cc0e6d7944d5d2220
acharya221b <30840365+acharya221b@users.noreply.github.com>
2017-08-28 19:17:33 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,3 +16,29 @@ function moveRight ()
 img.onclick = function() {
  var interval = setInterval(moveRight,50);  
 };
+
+//Counter Code
+var button = document.getElementById('counter');
+var counter=0;
+button.onclick = function () {
+    
+  //Create a request object
+  var request =  new XMLHttpRequest();
+  
+  //Capture the response and store it in a variable
+  request.onreadystatechange = function () {
+      if(request.readyState === XMLHttpRequest.DONE) {
+          //Take some action
+          if(request.status == 200) {
+                request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+          }
+      }
+      //Not done yet
+  };
+  
+    //Make the request
+    request.open('GET', 'http://acharya221b.imad.hasura-app.io/counter', true);
+    request.send(null);
+};
