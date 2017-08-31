e568448e959e24eed8a593ab0bd9a353026f9c52
acharya221b <30840365+acharya221b@users.noreply.github.com>
2017-08-31 12:17:44 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,7 +20,6 @@ img.onclick = function() {
 
 //Counter Code
 var button = document.getElementById('counter');
-
 button.onclick = function () {
   //Create a request object
   var request =  new XMLHttpRequest();
@@ -44,19 +43,50 @@ button.onclick = function () {
 };
 
 //Submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
+
 submit.onclick = function () {
-  //Make a request to the server and send the name
+  //Create a request object
+  var request =  new XMLHttpRequest();
   
-  //Capture a list of names and render it as a list
+  //Capture the response and store it in a variable
+  request.onreadystatechange = function () {
+      if(request.readyState === XMLHttpRequest.DONE) {
+          //Take some action
+          if(request.status === 200) {
+                /*var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString(); */
+                //Capture a list of names and render it as a list
+                var names= request.responseText; //instead of var names=['name1', 'name2', 'name3', 'name4'];
+                //request.responseText is a string of names
+                 names= JSON.parse(names); //Converting string to Javascript array of objects
+                  var list='';
+                  for(var i=0; i < names.length; i++) {
+                      list += '<li>' + names[i] + '</li>';
+                  }
+                  var ul=document.getElementById('namelist');
+                  ul.innerHTML = list;
+          }
+      }
+      //Not done yet
+  };
+  
+    //Make the request
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
+    request.open('GET', 'http://acharya221b.imad.hasura-app.io/submit-name?name='+name, true); 
+    /*Instead of '/counter' we write /submit-name?name='+name*/
+    request.send(null);
+  
+ /* //Capture a list of names and render it as a list
   var names=['name1', 'name2', 'name3', 'name4'];
   var list='';
   for(var i=0; i < names.length; i++) {
-      list += '<li>' + names[i] + '</ li>';
+      list += '<li>' + names[i] + '</li>';
   }
   var ul=document.getElementById('namelist');
-  ul.innerHTML = list;
+  ul.innerHTML = list;*/
 };
 
