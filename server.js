d6ad92d3087fa9a31e324cc28f18ff5eb52dddbb
acharya221b <30840365+acharya221b@users.noreply.github.com>
2017-09-02 14:36:09 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
 var config = {
     user: 'acharya221b',
     database: 'acharya221b',
@@ -109,6 +110,19 @@ function createTemplate(data) {
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+function hash (input, salt) {
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return hashed.toString('hex');
+    //pbkdf2Sync is a function in default nodejs library crypto
+    //crypto.pbkdf2Sync(password,salt,iterations,keylen,digest);
+}
+
+app.get('/hash/:input', function (req, res) {
+   var hashedString = hash(req.params.input,'this-is-some-random-string'); //hash(input,salt);
+   res.send(hashedString);
+});
+
 var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
    //Make a select request.
