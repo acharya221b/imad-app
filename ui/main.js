847750ffba6cb5b920ffb827ec1ea8e0bdcf2fa4
acharya221b <30840365+acharya221b@users.noreply.github.com>
2017-09-03 13:01:53 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,45 @@
+var submit = document.getElementById('submit_btn');
+submit.onclick = function () {
+  //Create a request object
+  var request =  new XMLHttpRequest();
+  
+  //Capture the response and store it in a variable
+  request.onreadystatechange = function () {
+      if(request.readyState === XMLHttpRequest.DONE) {
+          //Take some action
+          if(request.status === 200) {
+                console.log('user logged in');
+                alert('Logged in successfully.');
+          } else if(request.status === 403) {
+              alert('Username/password incorrect');
+          } else if(request.status === 500) {
+              alert('Something went wrong on the server');
+          }
+      }
+      //Not done yet
+  };
+  
+  //Make the request
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST', 'http://acharya221b.imad.hasura-app.io/login', true); 
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({username: username, password: password}));
+  
+ /* //Capture a list of names and render it as a list
+  var names=['name1', 'name2', 'name3', 'name4'];
+  var list='';
+  for(var i=0; i < names.length; i++) {
+      list += '<li>' + names[i] + '</li>';
+  }
+  var ul=document.getElementById('namelist');
+  ul.innerHTML = list;*/
+};
+
+
+
 /*console.log('Loaded!');
 //Change the text of the main-text div
 var element = document.getElementById('main-text');
@@ -83,43 +125,4 @@ submit.onclick = function () {
   
   //Submit username/password to login
 
-var submit = document.getElementById('submit_btn');
-submit.onclick = function () {
-  //Create a request object
-  var request =  new XMLHttpRequest();
-  
-  //Capture the response and store it in a variable
-  request.onreadystatechange = function () {
-      if(request.readyState === XMLHttpRequest.DONE) {
-          //Take some action
-          if(request.status === 200) {
-                console.log('user logged in');
-                alert('Logged in successfully.');
-          } else if(request.status === 403) {
-              alert('Username/password incorrect');
-          } else if(request.status === 500) {
-              alert('Something went wrong on the server');
-          }
-      }
-      //Not done yet
-  };
-  
-  //Make the request
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-    console.log(username);
-    console.log(password);
-    request.open('POST', 'http://acharya221b.imad.hasura-app.io/login', true); 
-    request.setRequestHeader('Content-Type', 'application/json');
-    request.send(JSON.stringify({username: username, password: password}));
-  
- /* //Capture a list of names and render it as a list
-  var names=['name1', 'name2', 'name3', 'name4'];
-  var list='';
-  for(var i=0; i < names.length; i++) {
-      list += '<li>' + names[i] + '</li>';
-  }
-  var ul=document.getElementById('namelist');
-  ul.innerHTML = list;*/
-};
 
